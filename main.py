909a73aeeb3fb80784114831d7ad31c3b61cb8ec
MuchenLu <ryan.yonglai@gmail.com>
2026-04-11 13:40:33 +0800

    chore: 更新套件

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def main():
-    print("Hello from ai-for-better-future!")
+from models.detector import detect
+
+def main() :
+    try :
+        detect()
+    except KeyboardInterrupt :
+        pass
 
 
 if __name__ == "__main__":
